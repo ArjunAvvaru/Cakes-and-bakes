2f1d1698164afc5ae2562d580371fbe0cac60042
AvvaruArjun <33121128+AvvaruArjun@users.noreply.github.com>
2021-09-11 15:39:27 +0530

    Add files via upload

--- UI 1/scripts/scripts.js
+++ UI 1/scripts/scripts.js
@@ -1,5 +1,15 @@
 $( document ).ready(function() {
     $(".top-header-cart").on("click",function() {
     $(".cart-contents").fadeToggle("fast", "swing");
+    console.log("Hi1");
+    });
+    
+
+    $(document).on('click', function(e) {
+        var container = $(".cart-complete");
+        var ChildCont = $(".cart-complete .cart-contents");
+        if (!$(e.target).closest(container).length) {
+            ChildCont.hide();
+        }
     });
 });
